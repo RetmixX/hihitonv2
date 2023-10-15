1a80a2562971cfaff2d5c7c7b4e2f22f5fad6661
retmix <jameslayng123@gmail.com>
2023-10-15 13:54:26 +0700

    Add swagger

--- backend/project/src/main.rs
+++ backend/project/src/main.rs
@@ -1,26 +1,44 @@
 mod utils;
 
-use actix_web::{App, HttpResponse, HttpServer, Responder, web};
+use actix_web::{App, get, HttpResponse, HttpServer, Responder, web};
+use utoipa::OpenApi;
+use utoipa_swagger_ui::SwaggerUi;
+use crate::utils::documentation::SwaggerDoc;
+use crate::utils::responses::MessageResponse;
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    println!("ALLO");
+    println!("Start server");
     run().await
 }
 
-async fn healthcheck() -> impl Responder {
-    HttpResponse::Ok().body("Work!")
+#[utoipa::path(
+    get,
+    path = "/api/healthcheck",
+    responses(
+        (
+            status = 200,
+            body = MessageResponse
+        )
+    )
+)]
+#[get("/healthcheck")]
+pub async fn healthcheck() -> impl Responder {
+    HttpResponse::Ok().json(MessageResponse::new("Ok"))
 }
 
 
-
 async fn run() -> std::io::Result<()> {
+    let swagger = SwaggerDoc::openapi();
     HttpServer::new(move || {
         App::new()
             .default_service(web::route().to(not_found))
-            .service(web::scope("/api")
-                .route("/healthcheck", web::get().to(healthcheck))
+            .service(
+                SwaggerUi::new("/swagger-ui/{_:.*}")
+                    .url("/api-docs/openapi.json", swagger.clone())
             )
+            .service(web::scope("/api")
+                .service(healthcheck))
     }).bind(("0.0.0.0", 8000))?.run().await
 }
 
